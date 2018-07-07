b5412110111a816cf2a4dbc061512d2466e5c278
Jared <selling.jared@gmail.com>
2018-07-07 10:36:55 -0400

    Connect to Spring boot

---
+++ src/main/frontend/src/app/tasks/task.service.ts
@@ -0,0 +1,16 @@
+
+import { map } from 'rxjs/operators';
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable()
+export class TaskService {
+
+    constructor(
+        private http: HttpClient
+    ) {}
+
+    getTasks() {
+        return this.http.get("/api/tasks");
+    }
+}
--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Task } from '../task.model';
+import {TaskService} from "../task.service";
+import {forEach} from "@angular/router/src/utils/collection";
+
 @Component({
   selector: 'app-tasks-list',
   templateUrl: './tasks-list.component.html',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksListComponent implements OnInit {
 
-  constructor() { }
+    tasks: Task[] = [];
 
-  ngOnInit() {
-  }
+    constructor(
+        private taskService: TaskService
+    ) { }
+
+    ngOnInit() {
+        return this.taskService.getTasks()
+            .subscribe(
+                (tasks: any[]) => {
+                    this.tasks = tasks;
+                },
+                (error) => console.log(error)
+            );
+    }
+
+    getDueDateBadge(task: Task) {
+        return task.completed ? 'badge-success' : 'badge-primary';
+    }
+
+    onTaskChange(event, task) {
+        console.log('Task has changed.');
+    }
 
 }
